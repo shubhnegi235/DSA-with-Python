339183c99dbf6485f14225ec5ae641b1b0d1b01e
shubhnegi235 <shubhnegi57@gmail.com>
2026-07-01 11:08:52 +0530

    Day-6 completed

---
+++ Day-6/occurrence.py
@@ -0,0 +1,19 @@
+def foccurrence(arr, target):
+    low=0
+    high=len(arr)-1
+    ans=-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]==target:
+            ans=mid
+            high=mid-1
+        elif arr[mid]<target:
+            low=mid+1
+        else:
+            high-mid-1
+    return ans
+
+arr=[2,4,4,4,7,9,11]
+target=4
+print(foccurrence(arr,target))
+
